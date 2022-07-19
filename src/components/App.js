3bfcfd60f22a3e8e4f91f453e193f47624919689
nehemias-bit <nemicruz003@gmail.com>
2022-07-18 22:54:28 -0400

    Created Dropdown component

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,43 @@
-import React from 'react';
-import Accordion from './Accordion';
-import Search from './Search';
+import React, { useState } from 'react';
+// import Accordion from './Accordion';
+// import Search from './Search';
+import Dropdown from './Dropdown';
+
+const options = [{
+  label: 'The Color Red',
+  value: 'red'
+}, {
+  label: 'The Color Green',
+  value: 'blue'
+}, {
+  label: 'A Shade of Blue',
+  value: 'Blue'
+}];
+
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'React is a front end Javascript framework'
+//   },
+//   {
+//     title: 'Why React?',
+//     content: 'React is a favorite JS library among engineers'
+//   },
+//   {
+//     title: 'How React?',
+//     content: 'You use React by creating components'
+//   }];
 
 const App = () => {
 
-  const items = [
-    {
-      title: 'What is React?',
-      content: 'React is a front end Javascript framework'
-    },
-    {
-      title: 'Why React?',
-      content: 'React is a favorite JS library among engineers'
-    },
-    {
-      title: 'How React?',
-      content: 'You use React by creating components'
-    }];
+  const [selected, setSelected] = useState(options[0]);
 
   return (
     <div>
-      {/* <Accordion items={items} /> */}
-      <Search />
+      <Dropdown
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected} />
     </div>
   );
 };
